1ed3dc37728be85d9feb354a4acb2b137183db7f
Esteban Carranza <esteban.carranza@outlook.com>
2018-01-26 23:46:02 -0600

    fff
    tt

---
+++ Assets/scripts/ecPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ecPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	float speed = 10;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+}
