5414577ec40f239f917288551bc34439176d339f
RANGEL\JoséAntonio <jarz_1995@hotmail.com>
2018-01-27 10:47:01 -0600

    Ignoren esto
    Ignoren esto :v

---
+++ Assets/JARZ_scripts/RangeNotVis.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeNotVis : MonoBehaviour {
+
+	//private SpriteRenderer rangeSpriteRender;
+	// Use this for initialization
+	void Start () {
+		//SpriteRenderer rangeSpriteRender;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		//rangeSpriteRender.enabled = false;
+	}
+
+}
---
+++ Assets/JARZ_scripts/Torre_Disparo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torre_Disparo : MonoBehaviour {
+
+	//List<GameObject> targetedEnemies;
+
+	//private jarz_enemy target;
+	//private Queue<jarz_enemy> enemy = new Queue<jarz_enemy> ();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void OnTriggerStay(Collider other){
+		//enemy.
+		if(other.gameObject.CompareTag("Enemy")){
+			//targetedEnemies.Add (other.gameObject);
+		//	target = other.GetComponent<Enemy>();
+		}
+
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.CompareTag("Enemy")) {
+			//targetedEnemies.Remove (other.gameObject);
+		}
+	}
+
+	public float speed = 20;
+	// Update is called once per frame
+
+	void Update () {
+		
+		
+		/*if(Input.GetMouseButtonDown (0)){
+			if(Input.GetKey (KeyCode.LeftArrow))
+				transform.Rotate (Vector3.forward, speed*Time.deltaTime);
+			if (Input.GetKey (KeyCode.RightArrow))
+				transform.Rotate (Vector3.forward, -speed*Time.deltaTime);
+			
+		}*/
+	}
+}
