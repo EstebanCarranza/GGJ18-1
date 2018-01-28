0b31d45d183f9871ef05042af51657c2a7d30095
RANGEL\JoséAntonio <jarz_1995@hotmail.com>
2018-01-27 20:09:01 -0600

    Cooldown
    Cooldown para las armas.

--- Assets/ecScripts/ecTorre.cs
+++ Assets/ecScripts/ecTorre.cs
@@ -13,6 +13,25 @@ public class ecTorre : MonoBehaviour {
 	public GameObject torre5;
 	public GameObject torre6;
 
+	/*public bool usoTorre1 = 0;
+	public bool usoTorre2 = 0;
+	public bool usoTorre3 = 0;
+	public bool usoTorre4 = 0;
+	public bool usoTorre5 = 0;
+	public bool usoTorre6 = 0;*/
+
+	public int torreta = 1;
+
+	public float secs = 0;
+
+	public float coolDown = 8;
+
+	public float coolDownTimer1 = 0;
+	public float coolDownTimer2 = 0;
+	public float coolDownTimer3 = 0;
+	public float coolDownTimer4 = 0;
+	public float coolDownTimer5 = 0;
+	public float coolDownTimer6 = 0;
 
 	void Start () {
 		
@@ -37,20 +56,131 @@ public class ecTorre : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
+		if (coolDownTimer1 < 0) {
+			coolDownTimer1 = 0;
+		}
+		if (coolDownTimer1 > 0) {
+			coolDownTimer1 -= Time.deltaTime;
+		}
+
+		if (coolDownTimer2 < 0) {
+			coolDownTimer2 = 0;
+		}
+		if (coolDownTimer2 > 0) {
+			coolDownTimer2 -= Time.deltaTime;
+		}
+
+		if (coolDownTimer3 < 0) {
+			coolDownTimer3 = 0;
+		}
+		if (coolDownTimer3 > 0) {
+			coolDownTimer3 -= Time.deltaTime;
+		}
+
+		if (coolDownTimer4 < 0) {
+			coolDownTimer4 = 0;
+		}
+		if (coolDownTimer4 > 0) {
+			coolDownTimer4 -= Time.deltaTime;
+		}
+
+		if (coolDownTimer5 < 0) {
+			coolDownTimer5 = 0;
+		}
+		if (coolDownTimer5 > 0) {
+			coolDownTimer5 -= Time.deltaTime;
+		}
+
+		if (coolDownTimer6 < 0) {
+			coolDownTimer6 = 0;
+		}
+		if (coolDownTimer6 > 0) {
+			coolDownTimer6 -= Time.deltaTime;
+		}
 	
 		if (Input.GetKey (KeyCode.LeftArrow)) {izqDer = true;}
 		if (Input.GetKey (KeyCode.RightArrow)) {izqDer = false;}
-		if (Input.GetKey (KeyCode.Q)) {rotate_object (torre1, izqDer);}
-		if (Input.GetKey (KeyCode.A)) {rotate_object (torre2, izqDer);}
-		if (Input.GetKey (KeyCode.S)) {rotate_object (torre3, izqDer);}
-		if (Input.GetKey (KeyCode.D)) {rotate_object (torre4, izqDer);}
-		if (Input.GetKey (KeyCode.E)) {rotate_object (torre5, izqDer);}
-		if (Input.GetKey (KeyCode.W)) {rotate_object (torre6, izqDer);}
 
+		/*if (Input.GetKey (KeyCode.Q) ) {rotate_object (torre1, izqDer); secs = 60;}
+		else if (Input.GetKey (KeyCode.A)) {rotate_object (torre2, izqDer); secs = 60;}
+		else if (Input.GetKey (KeyCode.S) ) {rotate_object (torre3, izqDer); secs = 60;}
+		else if (Input.GetKey (KeyCode.D) ) {rotate_object (torre4, izqDer); secs = 60;}
+		else if (Input.GetKey (KeyCode.E) ) {rotate_object (torre5, izqDer); secs = 60;}
+		else if (Input.GetKey (KeyCode.W) ) {rotate_object (torre6, izqDer); secs = 60;}*/
+
+		switch (torreta) {
+		case 1:
+			if (Input.GetKey (KeyCode.Q) && coolDownTimer1 == 0) {	
+				rotate_object (torre1, izqDer);
+				coolDownTimer1 = coolDown; } 
+			else	torreta = 0;
+			break;
+		case 2:
+			if (Input.GetKey (KeyCode.A) && coolDownTimer2 == 0) {	
+				rotate_object (torre2, izqDer);
+				coolDownTimer2 = coolDown; } 
+			else	torreta = 0;
+			break;
+		case 3:
+			if (Input.GetKey (KeyCode.S) && coolDownTimer3 == 0) {	
+				rotate_object (torre3, izqDer);
+				coolDownTimer3 = coolDown; }
+			else	torreta = 0;
+			break;
+		case 4:
+			if (Input.GetKey (KeyCode.D) && coolDownTimer4 == 0) {	
+				rotate_object (torre4, izqDer);
+				coolDownTimer4 = coolDown; } 
+			else	torreta = 0;
+			break;
+		case 5:
+			if (Input.GetKey (KeyCode.E) && coolDownTimer5 == 0) {	
+				rotate_object (torre5, izqDer);
+				coolDownTimer5 = coolDown; } 
+			else	torreta = 0;
+			break;
+		case 6:
+			if (Input.GetKey (KeyCode.W) && coolDownTimer6 == 0) {
+				rotate_object (torre6, izqDer);
+				coolDownTimer6 = coolDown; } 
+			else	torreta = 0;
+			break;
+		default:
+			if (Input.GetKey (KeyCode.Q) && coolDownTimer1 > 5) {
+				rotate_object (torre1, izqDer);
+			} else if (Input.GetKey (KeyCode.Q))
+				torreta = 1;
+			if (Input.GetKey (KeyCode.A) && coolDownTimer2 > 5) {
+				rotate_object (torre2, izqDer); 
+			} else if (Input.GetKey (KeyCode.A))
+				torreta = 2;
+			if (Input.GetKey (KeyCode.S) && coolDownTimer3 > 5) {
+				rotate_object (torre3, izqDer); 
+			} else if (Input.GetKey (KeyCode.S))
+				torreta = 3;
+			if (Input.GetKey (KeyCode.D) && coolDownTimer4 > 5) {
+				rotate_object (torre4, izqDer); 
+			} else if (Input.GetKey (KeyCode.D))
+				torreta = 4;
+			if (Input.GetKey (KeyCode.E) && coolDownTimer5 > 5) {
+				rotate_object (torre5, izqDer); 
+			} else if (Input.GetKey (KeyCode.E))
+				torreta = 5;
+			if (Input.GetKey (KeyCode.W) && coolDownTimer6 > 5) {
+				rotate_object (torre6, izqDer); 
+			} else if (Input.GetKey (KeyCode.W))
+				torreta = 6;
+
+			break;
+		}
+
+		if (secs > 0) {	secs--; }
 	}
 
 	public Vector3 center;
 	public GameObject bala;
+
 	void spawn_balas()
 	{
 		Vector3 pos;
