ae5895e04dccf284a0926c4267b87cd6613d3cd0
Francisco Daniel <francisco.ochoa@appsdigitales.com>
2018-01-27 20:12:01 -0600

    Se implemento el menu radial
    Se implemento el menu radial

---
+++ Assets/ecScripts/RadialButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class RadialButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Image circle;
+	public Image icon;
+	public string title;
+	public RadialMenu myMenu;
+	Color defaultColor;
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		myMenu.selected = this;
+		defaultColor = circle.color;
+		circle.color = Color.white;
+	}
+
+
+
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		myMenu.selected = null;
+		circle.color = defaultColor;
+	}
+
+}
